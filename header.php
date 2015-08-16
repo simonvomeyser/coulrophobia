2fdbc78d5a65a5a70e5bb768662efb88a4ccaa54
Simon <simon.vom.eyser@gmail.com>
2015-08-16 10:50:35 +0200

    Removes underscores since styles where not good

--- header.php
+++ header.php
@@ -20,7 +20,6 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'coulrophobia' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="container">
