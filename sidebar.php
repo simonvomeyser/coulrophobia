54306167f8c041bcdb8d3499141046a2c8102968
Simon vom Eyser <simon.v.e@gmx.net>
2015-08-26 08:59:29 +0200

    Belongs to last commit

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div class="col-12 col-lg-4" id="sidebar">
+<div class="col-md-4" id="sidebar">
 	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 			<?php dynamic_sidebar( 'sidebar' ); ?>
 	<?php endif; ?>
