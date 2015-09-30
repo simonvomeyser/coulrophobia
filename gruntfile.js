f6366aecce0fd0b709ba181d02e382f5199c46eb
Simon <simon.vom.eyser@gmail.com>
2015-09-30 11:55:21 +0200

    Big grunt intergration, cssfiles are minified and combined in watch task

--- gruntfile.js
+++ gruntfile.js
@@ -1,28 +1,46 @@
 module.exports = function(grunt) {
 
-// grunt.registerTask('speak', function() {
-// 	console.log ("Grunt working mate "); //Debug
-// });
+    // grunt.registerTask('speak', function() {
+    // 	console.log ("Grunt working mate "); //Debug
+    // });
 
-// grunt.registerTask('yell', function() {
-// 	console.log ("GRUNT WORKING MATE "); //Debug
-// });
+    // grunt.registerTask('yell', function() {
+    // 	console.log ("GRUNT WORKING MATE "); //Debug
+    // });
 
-// grunt.registerTask('default', ['speak', 'yell']);
+    // grunt.registerTask('default', ['speak', 'yell']);
 
-	grunt.initConfig({
-	  concat: {
-	    dist: {
-	      src: ['css/style.css',
-		      	'css/normalize.css',
-		      	'lib/bootstrap/css/bootstrap.min.css',
-		      	'lib/lightbox/css/lightbox.css',
-		      	'css/originalStyles.css',
-		      	'css/custom.css'],
-	      dest: 'style.css',
-	    },
-	  },
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.initConfig({
+        cssmin: {
+            target: {
+                options: {
+                    'keepSpecialComments': '0',
+                },
+                files: {
+                    'style-min.css': [
+                        'css/normalize.css',
+                        'lib/bootstrap/css/bootstrap.min.css',
+                        'lib/lightbox/css/lightbox.css',
+                        'css/original.css',
+                        'css/custom.css'
+                    ]
+                },
+            },
+        },
+        concat: {
+            dist: {
+                src: ['css/wordpressThemeComment.css', 'css/style-min.css'],
+                dest: 'style.css',
+            },
+        },
+        watch: {
+            css: {
+                files: ['css/*.css'],
+                tasks: ['cssmin', 'concat']
+            },
+        },
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 };
