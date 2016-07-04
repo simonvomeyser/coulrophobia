ba43724fa2dfbf6def2321ce0f2070d1271e446f
Simon vom Eyser <simon.vom.eyser@gmail.com>
2016-07-04 20:59:45 +0200

    bugfix - add sold out span again

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -35,7 +35,9 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_product_link_open - 10
 	 */
 	do_action( 'woocommerce_before_shop_loop_item' );
-
+	if ( !$product->is_in_stock()) {
+		echo '<span class="soldout">SOLD OUT!</span>'; 
+	}
 	/**
 	 * woocommerce_before_shop_loop_item_title hook.
 	 *
