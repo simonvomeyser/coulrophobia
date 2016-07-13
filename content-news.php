4f97c230b5d579f58b8826a6198bb7defd51d8bb
Simon vom Eyser <simon.vom.eyser@gmail.com>
2016-07-13 07:13:49 +0200

    SEO - Added HTML5 Elements like article and aside

--- content-news.php
+++ content-news.php
@@ -3,50 +3,52 @@
 Template for a single news item
  */
 ?>
-<div class="coulrophobia-news-item">
+<article>
+	<div class="coulrophobia-news-item">
 
-	<!-- header -->
-	<div class="coulrophobia-news-header">
-		<div>
-			<span class="coulrophobia-news-date">
-				<?php the_time('j F Y'); ?>
-			</span>
-		</div>
-		<h3 class="no-top-margin">
-			<a href="<?php echo wp_get_attachment_url(get_post_thumbnail_id( $post_id ))?>" data-lightbox="<?php the_title(); ?>" data-title="<?php the_title(); ?>">
-				<?php the_title() ?>
-			</a>
-		</h3>
-	</div>
-
-	<div class="coulrophobia-news-content <?php if(get_the_post_thumbnail()) echo "coulrophobia-news-content-thumbnail-padding"; ?>">
-
-			<?php if(get_the_post_thumbnail()): ?>
-			<div class="coulrophobia-news-img-wrap">
-
+		<!-- header -->
+		<div class="coulrophobia-news-header">
+			<div>
+				<span class="coulrophobia-news-date">
+					<?php the_time('j F Y'); ?>
+				</span>
+			</div>
+			<h3 class="no-top-margin">
 				<a href="<?php echo wp_get_attachment_url(get_post_thumbnail_id( $post_id ))?>" data-lightbox="<?php the_title(); ?>" data-title="<?php the_title(); ?>">
-					<?php the_post_thumbnail('thumbnail'); ?>
+					<?php the_title() ?>
 				</a>
+			</h3>
+		</div>
+
+		<div class="coulrophobia-news-content <?php if(get_the_post_thumbnail()) echo "coulrophobia-news-content-thumbnail-padding"; ?>">
+
+				<?php if(get_the_post_thumbnail()): ?>
+				<div class="coulrophobia-news-img-wrap">
+
+					<a href="<?php echo wp_get_attachment_url(get_post_thumbnail_id( $post_id ))?>" data-lightbox="<?php the_title(); ?>" data-title="<?php the_title(); ?>">
+						<?php the_post_thumbnail('thumbnail'); ?>
+					</a>
+
+				</div>
+				<?php endif; ?>
+
+
+			<!-- teas=ser text -->
+			<div class="coulrophobia-news-teaser-text">
+
+				<?php  echo the_content() ?>
 
 			</div>
-			<?php endif; ?>
-
-
-		<!-- teas=ser text -->
-		<div class="coulrophobia-news-teaser-text">
-
-			<?php  echo the_content() ?>
 
 		</div>
 
+		<!-- footer information -->
+		<div class="coulrophobia-news-footer">
+			<p>
+				<span class="coulrophobia-news-author">
+					erstellt von <?php the_author() ?>
+				</span>
+			</p>
+		</div>
 	</div>
-
-	<!-- footer information -->
-	<div class="coulrophobia-news-footer">
-		<p>
-			<span class="coulrophobia-news-author">
-				erstellt von <?php the_author() ?>
-			</span>
-		</p>
-	</div>
-</div>
+</article>
--- front-page.php
+++ front-page.php
@@ -7,51 +7,55 @@ get_header(); ?>
 
 
 <div class="col-md-8">
-	<div id="maincontent">
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php the_content(); ?>
-
-			<?php endwhile; ?>
+	<section class="section-maincontent">
+		<div id="maincontent">
+			<?php if ( have_posts() ) : ?>
+	
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+	
+					<?php the_content(); ?>
+	
+				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+	
+			<?php endif; ?>
+	
+			<?php
+				$args = array(
+					'post_type' 	=> 'post',
+					'posts_per_page' 	=> 3,
+					);
+				$query = new WP_Query($args);
+			?>
+			<?php if ( have_posts() ) : ?>
+	
+				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+	
+					<?php get_template_part('content', 'news'); ?>
+	
+				<?php endwhile; ?>
+	
+			<?php else : ?>
+	
+				<p>Keine News gefunden</p>
+	
+			<?php endif; ?>
 			<?php wp_reset_postdata(); ?>
-
-		<?php endif; ?>
-
-		<?php
-			$args = array(
-				'post_type' 	=> 'post',
-				'posts_per_page' 	=> 3,
-				);
-			$query = new WP_Query($args);
-		?>
-		<?php if ( have_posts() ) : ?>
-
-			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-
-				<?php get_template_part('content', 'news'); ?>
-
-			<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<p>Keine News gefunden</p>
-
-		<?php endif; ?>
-		<?php wp_reset_postdata(); ?>
-		<div class="coulrophobia-news-more text-right" >
-			<a href ="<?php echo site_url() .'/news'; ?>">Alle News anzeigen <i class="fa fa-angle-double-right"></i></a>
+			<div class="coulrophobia-news-more text-right" >
+				<a href ="<?php echo site_url() .'/news'; ?>">Alle News anzeigen <i class="fa fa-angle-double-right"></i></a>
+			</div>
 		</div>
+	</section>
+</div>
+<aside>
+	<div class="col-md-4" id="frontpage-sidebar">
+		<?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
+			<div id="primary-sidebar" class="primary-sidebar widget-area">
+				<?php dynamic_sidebar( 'home_right_1' ); ?>
+			</div><!-- #primary-sidebar -->
+		<?php endif; ?>
 	</div>
-</div>
-<div class="col-md-4" id="frontpage-sidebar">
-	<?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
-		<div id="primary-sidebar" class="primary-sidebar widget-area">
-			<?php dynamic_sidebar( 'home_right_1' ); ?>
-		</div><!-- #primary-sidebar -->
-	<?php endif; ?>
-</div>
+</aside>
 
 <?php get_footer(); ?>
