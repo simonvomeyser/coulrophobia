4db41c2e075c3b100c0051767e7cf1e0f6b0158a
Simon vom Eyser <simon.vom.eyser@gmail.com>
2016-07-13 07:06:38 +0200

    consistent indention

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
  */
 
 if ($_COOKIE['iwashere'] != "yes") {
-	setcookie("iwashere", "yes", time()+315360000);
+    setcookie("iwashere", "yes", time()+315360000);
 }
 
 ?><!DOCTYPE html>
@@ -33,69 +33,69 @@ if ($_COOKIE['iwashere'] != "yes") {
 <div id="trans-bg" style="background-color: #<?php background_color(); ?>;"></div>
 
 <?php if (is_front_page()): ?>
-		<div id="bg-fade-in" style="background-color: #<?php background_color(); ?>"></div>
+        <div id="bg-fade-in" style="background-color: #<?php background_color(); ?>"></div>
 <?php endif ?>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="container">
-		    <div id="logo">
-		    	<h1>
-		        	<a href="<?php echo site_url(); ?>">
-		        		<img class="img-responsive"
-		        		width="420"
-		        		src="<?php echo( get_header_image() ); ?>"
-		        		alt="<?php if ( is_single() ) {
-						        single_post_title('', true); 
-						    } else {
-						        bloginfo('name'); echo " - "; bloginfo('description');
-						    }
-						    ?>" />
-		        	</a>
-		        </h1>
-		    </div>
-		</div>
+    <header id="masthead" class="site-header" role="banner">
+        <div class="container">
+            <div id="logo">
+                <h1>
+                    <a href="<?php echo site_url(); ?>">
+                        <img class="img-responsive"
+                        width="420"
+                        src="<?php echo( get_header_image() ); ?>"
+                        alt="<?php if ( is_single() ) {
+                                single_post_title('', true); 
+                            } else {
+                                bloginfo('name'); echo " - "; bloginfo('description');
+                            }
+                            ?>" />
+                    </a>
+                </h1>
+            </div>
+        </div>
 
-		<div class="container">
+        <div class="container">
 
-		    <div class="row">
-		        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg-12">
 
-					<nav class="navbar navbar-default" role="navigation">
-						<div class="navbar-header">
-						  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
-						    <span class="sr-only">Toggle navigation</span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-						  </button>
-						</div>
-						<?php
+                    <nav class="navbar navbar-default" role="navigation">
+                        <div class="navbar-header">
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                        </div>
+                        <?php
 
 
 
-						?>
-					<?php /* Primary navigation */
-					wp_nav_menu( array(
-						'menu'            => '',
-						'container'       => 'div',
-						'container_id'    => 'bs-example-navbar-collapse-2',
-						'container_class' => 'collapse navbar-collapse',
-						'menu_class'      => 'menu',
-						'theme_location'  => 'primary-menu',
-						'menu_class'      => 'nav navbar-nav',
-						'menu_id'         => '',
-						'depth'        	  => '3',
-					  //Process nav menu using our custom nav walker
-					  'walker' => new wp_bootstrap_navwalker())
-					);
-					?>
-					</nav>
-		        </div>
-		    </div>
+                        ?>
+                    <?php /* Primary navigation */
+                    wp_nav_menu( array(
+                        'menu'            => '',
+                        'container'       => 'div',
+                        'container_id'    => 'bs-example-navbar-collapse-2',
+                        'container_class' => 'collapse navbar-collapse',
+                        'menu_class'      => 'menu',
+                        'theme_location'  => 'primary-menu',
+                        'menu_class'      => 'nav navbar-nav',
+                        'menu_id'         => '',
+                        'depth'           => '3',
+                      //Process nav menu using our custom nav walker
+                      'walker' => new wp_bootstrap_navwalker())
+                    );
+                    ?>
+                    </nav>
+                </div>
+            </div>
 
-		</div>
+        </div>
 
-	</header><!-- #masthead -->
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content container <?php if (is_front_page()) {echo 'content-fade-in';} ?>">
-	<div class="row">
+    <div id="content" class="site-content container <?php if (is_front_page()) {echo 'content-fade-in';} ?>">
+    <div class="row">
